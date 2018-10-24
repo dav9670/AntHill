5f969d31d8901cc94a2f97d58b9c6f2257943699
david <david.lafreniere@live.ca>
2018-10-24 09:59:01 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main(Main.class.getName());
+    }
+
+    @Override
+    public void settings() {
+        super.settings();
+        size(800, 800, P2D);
+    }
+
+    @Override
+    public void draw() {
+        
+    }
+}
